41bb8ca12f38e6a9638df2d0037cc3b5c9b3b87b
Vinuthna Surampudi <vinuthnas@usf.edu>
2021-05-28 01:05:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment1_Summer2021
+{
+    using System;
+    class GFG
+    {
+
+        // Returns true if the string
+        // is pangram else false
+        public static bool checkIfPangram(string str)
+        {
+
+            // Create a hash table to mark the
+            // characters present in the string
+            // By default all the elements of
+            // mark would be false.
+            bool[] mark = new bool[26];
+
+            // For indexing in mark[]
+            int index = 0;
+
+            // Traverse all characters
+            for (int i = 0; i < str.Length; i++)
+            {
+                // If uppercase character, subtract 'A'
+                // to find index.
+                if ('A' <= str[i] && str[i] <= 'Z')
+                    index = str[i] - 'A';
+
+                // If lowercase character,
+                // subtract 'a' to find
+                // index.
+                else if ('a' <= str[i] && str[i] <= 'z')
+                    index = str[i] - 'a';
+
+                // If this character is other than english
+                // lowercase and uppercase characters.
+                else
+                    continue;
+
+                mark[index] = true;
+            }
+
+            // Return false if any
+            // character is unmarked
+            for (int i = 0; i <= 25; i++)
+                if (mark[i] == false)
+                    return (false);
+
+            // If all characters
+            // were present
+            return (true);
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            string str = "The quick brown fox jumps over the lazy";
+
+            if (checkIfPangram(str) == true)
+                Console.WriteLine(str + " is a pangram.");
+            else
+                Console.WriteLine(str + " is not a pangram.");
+        }
+    }
+
+
+
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+            
