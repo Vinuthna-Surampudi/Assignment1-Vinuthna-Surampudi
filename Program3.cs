542ddf2b0b0fae9b3864d3bafe0f8d8d609373cb
Vinuthna-Surampudi <84541999+Vinuthna-Surampudi@users.noreply.github.com>
2021-05-28 03:21:02 -0400

    Question 3

---
+++ Program3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Question_3
+{
+    class Question3
+    {
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int ans = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 1, 1, 3 };
+            int gp = Question3.NumIdenticalPairs(nums);
+            Console.WriteLine("Q3:");
+            Console.WriteLine("The number of IdenticalPairs in the array are {0}:", gp);
+        }
+    }
+}
